27da4b8876ebb6d5a79e5d895382f27933d02d00
Shawn K Lewis <shawnklewis@outlook.com>
2020-02-02 23:53:05 -0400

    works great

--- BME280Logger/Program.cs
+++ BME280Logger/Program.cs
@@ -29,6 +29,30 @@ namespace BME280Logger {
             //UnixI2cDevice _i2c = new UnixI2cDevice(_i2cConnectionSettings);
             //BME280Driver _bme280 = new BME280Driver(_i2c);
 
+            DateTime _lastLog = DateTime.MinValue;
+            string _connectionString = string.Empty;
+            if (File.Exists("connectionstring.txt"))
+            {
+                _connectionString = File.ReadAllText("connectionstring.txt");
+                Console.WriteLine("Connection string found. Connecting to DB...");
+            }
+            else
+            {
+                Console.WriteLine("connectionstring.txt not found. exiting");
+                Environment.Exit(45); //connection string not found
+            }
+
+            SqlConnection _con = new SqlConnection(_connectionString);
+            try
+            {
+                _con.Open();
+                Console.WriteLine($"Connected to Server \"{_con.DataSource}\", and database \"{_con.Database}\"");
+            }
+            catch(SqlException ex)
+            {
+                Console.WriteLine($"Unable to connect using :\"{_connectionString}\" exiting");
+                throw ex;
+            }
 
             _bme280.Initialize();
 
@@ -39,33 +63,28 @@ namespace BME280Logger {
                 BME280OverSample.X1,
                 BME280Filter.Off);
 
-            DateTime _lastLog = DateTime.MinValue;
-            string _connectionString = string.Empty;
-            if (File.Exists("connectionstring.txt"))
-            {
-                _connectionString = File.ReadAllText("connectionstring.txt");
-            }
-            else
-            {
-                Console.WriteLine("connectionstring.txt not found. exiting");
-                return;
-            }
-
-            SqlConnection _con = new SqlConnection(_connectionString);
-            _con.Open();
             SqlCommand _command = new SqlCommand(Resources.InsertCommand, _con);
-            _command.Parameters.Add(new SqlParameter("Source", "Source1"));
+            _command.Parameters.Add(new SqlParameter("Source", File.Exists("source.txt") ? File.ReadAllText("source.txt") : "Source1"));
             _command.Parameters.Add(new SqlParameter("Id", System.Data.SqlDbType.UniqueIdentifier));
             _command.Parameters.Add(new SqlParameter("Barometric", System.Data.SqlDbType.Decimal));
             _command.Parameters.Add(new SqlParameter("Humidity", System.Data.SqlDbType.Decimal));
             _command.Parameters.Add(new SqlParameter("Temperature", System.Data.SqlDbType.Decimal));
-
+            int _loggingIntervalInSeconds = 60;
             while (true) {
                 _bme280.Read();
                 string _data = $"Pressure : {_bme280.Pressure:0.0} Pa, Humidity : {_bme280.Humidity:0.00}%, Tempreature : {_bme280.Temperature:0.00}°C";
                 string _logData = $"[{DateTime.Now.ToString("dddd MMM dd, yyyy h:mm:ss tt")}] {_data}";
 
-                if ((DateTime.Now - _lastLog).TotalMinutes > 1) {
+                if(_bme280.Temperature > 1 || _bme280.Humidity > 70)
+                {
+                    _loggingIntervalInSeconds = 10;
+                }
+                else
+                {
+                    _loggingIntervalInSeconds = 60;
+                }
+
+                if ((DateTime.Now - _lastLog).TotalSeconds > _loggingIntervalInSeconds) {
                     _lastLog = DateTime.Now;
                     File.AppendAllLines("BME280.log", new string[] { _logData });
                     Console.WriteLine(_logData);
@@ -73,7 +92,20 @@ namespace BME280Logger {
                     _command.Parameters["Barometric"].Value = _bme280.Pressure;
                     _command.Parameters["Humidity"].Value = _bme280.Humidity;
                     _command.Parameters["Temperature"].Value = _bme280.Temperature;
-                    _command.ExecuteNonQuery();
+                    try
+                    {
+                        if(_con.State!= System.Data.ConnectionState.Open)
+                        {
+                            _con.Open();
+                        }
+                        _command.ExecuteNonQuery();
+                        _con.Close();
+                    }
+                    catch(SqlException sqEx)
+                    {
+                        Console.WriteLine("Error executing query command");
+                        throw sqEx;
+                    }
                 }
                 else {
                     Console.WriteLine(_data);
